22723d478f186ee4db37b2ebcbfd85b59d3a282a
jsmith-exe <jamiesmith.exe@gmail.com>
2024-09-26 13:41:41 +0100

    Update

--- conversion.py
+++ conversion.py
@@ -2,8 +2,11 @@
 # Converter Integer to Binary format
 
 def int_to_binary(minterms, num_inputs):
+    
     minterm_binary_array = []
+
     for n in minterms:
+
         int_val = int(n)
         binary = bin(int_val)[2:].zfill(num_inputs)
 
--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import conversion as conv
 if __name__ == "__main__":
     try:
         num_inputs = int(input("Input the number of inputs: "))
+        if (num_inputs > 26):
+            print ("Error, number of inputs must be less than 26!")
+            exit()
     except ValueError:
         print ("Error: number of inputs must be an integer")
     
-    if (num_inputs > 26):
-        print("Error, number of inputs must be less than 26!")
-        exit()
-
     # Ensure consistent use of minterms_input
     minterms_input = input("Input the minterms as a comma-separated list (e.g., 1,2,3): ")
 
